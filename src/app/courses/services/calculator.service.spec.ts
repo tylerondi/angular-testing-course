7eade8781d438fcff2e54076c1be8c47d2a00665
Tyler Ondricek <tylerondi@gmail.com>
2020-06-08 15:57:29 -0600

    Add courses and calculator service unit tests

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,44 @@
+import { LoggerService } from './logger.service';
+import { CalculatorService } from './calculator.service';
+import { TestBed } from '@angular/core/testing';
+
+describe('CalculatorService', () => {
+
+  let calculator: CalculatorService,
+      loggerSpy: any;
+
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {provide: LoggerService, useValue: loggerSpy}
+      ]
+    });
+
+    calculator = TestBed.get(CalculatorService);
+  });
+
+  it('should add two numbers', () => {
+
+    const result = calculator.add(2, 2);
+
+    expect(result).toBe(4);
+
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+
+  });
+
+  it('should subtract two numbers', () => {
+
+    const result = calculator.subtract(2, 2);
+
+    expect(result).toBe(0, 'unexpected subtraction result');
+
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+
+  });
+
+});
